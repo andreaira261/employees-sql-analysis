c6631ef88c63ef1f2d3a3c77edb3ab653824c8db
Andrea <andreaia@uci.edu>
2023-08-29 14:22:41 -0700

    add sql files

---
+++ EmployeeSQL/employee_schema.sql
@@ -0,0 +1,94 @@
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(30)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(30)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+copy titles from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/titles.csv' delimiter ',' csv header;
+
+copy employees from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/employees.csv' delimiter ',' csv header;
+
+copy departments from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/departments.csv' delimiter ',' csv header;
+
+copy dept_emp from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/dept_emp.csv' delimiter ',' csv header;
+
+copy dept_manager from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/dept_manager.csv' delimiter ',' csv header;
+
+copy salaries from '/Users/andreaaguilar/sql-challenge/EmployeeSQL/data/salaries.csv' delimiter ',' csv header;
+
+select * from titles
+
+select * from employees
+
+select * from departments
+
+select * from dept_emp
+
+select * from dept_manager
+
+select * from salaries
